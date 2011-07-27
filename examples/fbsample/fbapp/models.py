daa1f5001859b2899fff87dd6c721f7ac928d181
Anna Warzecha <anna.warzecha@gmail.com>
2011-07-28 05:58:21 +0800

    Store Facebook UID as a string

--- examples/fbsample/fbapp/models.py
+++ examples/fbsample/fbapp/models.py
@@ -1,25 +1,12 @@
 from django.db import models
 
-def _2int(d, k):
-    try:
-        d = d.__dict__
-    except:
-        pass
-    
-    t = d.get(k, '')
-    if t == 'None':
-        t = 0
-    else:
-        t = int(t)
-    return t
-
 class UserManager(models.Manager):
     """Custom manager for a Facebook User."""
     
     def get_current(self, request):
         """Gets a User object for the logged-in Facebook user."""
         facebook = request.facebook
-        user, created = self.get_or_create(id=_2int(facebook, 'uid'))
+        user, created = self.get_or_create(id=facebook.uid)
         if created:
             # we could do some custom actions for new users here...
             pass
@@ -29,7 +16,7 @@ class User(models.Model):
     """A simple User model for Facebook users."""
 
     # Use the user's UID as the primary key in our database.
-    id = models.BigIntegerField(primary_key=True)
+    id = models.CharField(primary_key=True, max_length=50)
 
     # The data that you want to store for each user would go here.
     # For this sample, we let users let people know their favorite progamming
